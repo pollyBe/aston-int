4d3ff258c2c52b01c0e91d3f7ee1fb4d08acdce9
Polina Belash <polinka.zhuravlinka@gmail.com>
2025-09-04 16:28:58 +0300

    refactor: add changes according to review

--- my-app/src/main.tsx
+++ my-app/src/main.tsx
@@ -3,7 +3,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './app/App'
-import { ThemeProvider } from './shared/lib/theme/ThemeContext'
+import { ThemeProvider } from './shared/lib/theme/ThemeProvider'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
